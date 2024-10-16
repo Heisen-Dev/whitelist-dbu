deb6d5eb4fb5274eec4e2281c23bdb5abbe64f9b
Chrysstian <chryscuentadiscord@gmail.com>
2024-10-16 11:40:27 -0300

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -3,7 +3,7 @@ local users = {'YaEsta_EnUs0', 'chicles20191', '32Wex23', '23Wex32', 'derbexsaak
 	'TheHelllSpawn', 'XmacabreX20', 'sansYT2019', 'OfficialBagabundo','Gotenks_129', 'R4T4TOPP0', 'Bclayion', 'el_froti', 'MDLKingtails',
 	'PollySenko', 'Not_Bxnds', 'Benceke2', 'elsyerico13', 'Radiantprime23', 'Gonsi9', 'soynuevoxd6776','BORUTOBR1202', 'TheFinal126', 'pultaro',
 	'OhBruxo', 'Ohbruxinho', 'kula0_2alt', 'Kula0_2', 'Eldeom_38k', 'Rutra010201', 'HateNovaGod', 'IGOTBLACKBEARDOMG_S', 'BRUNOMAZUIM','JACK_RETALHADOR2',
-	'VikingaSpange', 'SumetRG', 'greenblademaster1', 'king_sombra9279', 'Davigame1319', 'borutousumake9', 'IHATEPIZZA221', 'TANJIRO129939', '0xHydroxide'
+	'VikingaSpange', 'SumetRG', 'greenblademaster1', 'king_sombra9279', 'Davigame1319', 'borutousumake9', 'IHATEPIZZA221', 'TANJIRO129939', '0xHydroxide','nepro375'
 }
 local isBuyer = false
 local me = game:GetService('Players').LocalPlayer
