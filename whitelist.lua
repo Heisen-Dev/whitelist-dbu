00672a610e728f3120caab2de016eecf2cdff35a
Heisen-Dev <ia.heisendev@gmail.com>
2024-10-16 08:11:29 -0600

    Create whitelist.lua

---
+++ whitelist.lua
@@ -0,0 +1,19 @@
+local users = {'YaEsta_EnUs0', 'chicles20191', '32Wex23', '23Wex32', 'derbexsaak', 'goyito132', 'princeofwales123', 'Tech_Sorcerer',
+	'Alternative930', 'TocinoProgramador', 'lioneutzuma', 'rtbl98', 'UNDERBOSS876', 'luckofnothing3', 'Lldkkdnnxn', 'Justforkicks99',
+	'TheHelllSpawn', 'XmacabreX20', 'sansYT2019', 'OfficialBagabundo','Gotenks_129', 'R4T4TOPP0', 'Bclayion', 'el_froti', 'MDLKingtails',
+	'PollySenko', 'Not_Bxnds', 'Benceke2', 'elsyerico13', 'Radiantprime23', 'Gonsi9', 'soynuevoxd6776','BORUTOBR1202', 'TheFinal126', 'pultaro',
+	'OhBruxo', 'Ohbruxinho', 'kula0_2alt', 'Kula0_2', 'Eldeom_38k', 'Rutra010201', 'HateNovaGod', 'IGOTBLACKBEARDOMG_S', 'BRUNOMAZUIM','JACK_RETALHADOR2',
+	'VikingaSpange', 'SumetRG', 'greenblademaster1', 'king_sombra9279', 'Davigame1319', 'borutousumake9', 'IHATEPIZZA221', 'TANJIRO129939', '0xHydroxide'
+}
+
+
+local me = game:GetService('Players').LocalPlayer
+
+
+
+
+for _, v in pairs(users) do
+	if string.lower(v) == string.lower(me.Name) then
+		print('Acceso')
+	end
+end
