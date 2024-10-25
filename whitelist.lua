1bfcafb67b5d1b244520b8eb47728f99fa02e233
DemonicMalware <lolgamer9005@yahoo.com>
2024-10-25 16:31:45 -0300

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -3,7 +3,7 @@ local users = {'YaEsta_EnUs0', 'chicles20191', '32Wex23', '23Wex32', 'derbexsaak
 	'TheHelllSpawn', 'XmacabreX20', 'sansYT2019', 'OfficialBagabundo', 'R4T4TOPP0', 'Bclayion', 'el_froti', 'MDLKingtails',
 	'PollySenko', 'Not_Bxnds', 'Benceke2', 'Radiantprime23', 'Gonsi9','BORUTOBR1202', 'TheFinal126', 'pultaro',
 	'OhBruxo', 'Ohbruxinho', 'kula0_2alt', 'Kula0_2', 'Bestboy0_07', 'Rutra010201', 'HateNovaGod', 'IGOTBLACKBEARDOMG_S', 'BRUNOMAZUIM','JACK_RETALHADOR2',
-	'VikingaSpange', 'greenblademaster1', 'king_sombra9279', 'Davigame1319', 'borutousumake9', 'IHATEPIZZA221', 'TANJIRO129939', '0xHydroxide', 
+	'VikingaSpange', 'greenblademaster1', 'king_sombra9279', 'Davigame1319', 'borutousumake9', 'IHATEPIZZA221', 'TANJIRO129939', 'testsec33', 
 	'nepro375', 'AnonymousDeicide', 'Silencemano4', 'Ziyadh_5', 'fran34577878', 'Thug786123', 'RehmXn12X', 'hairlinetaker', 'RehmXn12X', 'Thug786123',
 	'TomyT_T2', 'Arthemis018', 'SuperhotMax11', 'SSJYBz', 'NachoG17', 'VikingaNacke', 'JAVIER_R0BL0X', 'gtolooo',
 	'GameBoyIsBackNow', 'ch4pado291', 'Ox_Nan', 'EmperorDragon_84', 'RpgrGames02', 'omarfmx', 'Lol_EzGod', 'Victorgabrirj', 'habitat7722', 'xXLegendCrakXx', 'Azeldex7w7',
