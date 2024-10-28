9b78ca5664c7617aa539d2d25e33cb68ce74a8c6
Heisen-Dev <ia.heisendev@gmail.com>
2024-10-28 09:21:58 -0600

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -15,7 +15,8 @@ local users = {'YaEsta_EnUs0', 'chicles20191', '32Wex23', '23Wex32', 'derbex2', 
 	'soynuevoxd6776', 'AL_THINGX', 'snowmen0273482', 'anouar2015', 'Garymdomwkdmkwmdlwm', 'GigachadHD97', 'Caleb_thedemon098', 'pedrolucasalvesdo8z', 'pedrolucasalvesdo8z2',
 	'rrrujdirn', 'jiazaoli', 'Azeldex', 'coe_ninja', 'kingdomrobloxgaming', 'gabsi00', 'Frofro756', 'Douvegameplays999', 'Golytu', 'Paulonarro123', 'MIKEY_TMNN', 'elsyerico13', 'gabrielsfd123br', 'ruangx404',
 	'Zotrix443', 'borutousumake9', 'Lucasgerrard780', 'Marcio06l', 'amendoin313', 'ErrorGamer11', 'rafael_army9', 'Jeffelop2002', 'Subaru_Badass', 'bookiesalt2',
-	'Eoxjkosjf2', 'TheFruitU', 'ENASKURD21', 'deadshot2ez', 'gimmedb4', 'OfficialBagabundo', 'Sin_Liquid', 'IAGUSPRO_131', 'NeoZz_Acer', 'bqgamerr', 'Diamondkingez8'
+	'Eoxjkosjf2', 'TheFruitU', 'ENASKURD21', 'deadshot2ez', 'gimmedb4', 'OfficialBagabundo', 'Sin_Liquid', 'IAGUSPRO_131', 'NeoZz_Acer', 'bqgamerr', 'Diamondkingez8',
+	'LeGamerProVs'
 }
 
 
